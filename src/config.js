6680013f2b156aca7106a9c82cbb521b91b53b81
Tim Frazier <tim.b.frazier@gmail.com>
2020-11-04 20:49:33 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-tbf"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://qs9tt3zwc3.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_oe7yqSJ95",
+    APP_CLIENT_ID: "41m4bqnkbtoj7v2asft0tc7374",
+    IDENTITY_POOL_ID: "us-east-1:ad7409ce-5c92-410d-bb9c-4dc68f363416"
+  }
+};
